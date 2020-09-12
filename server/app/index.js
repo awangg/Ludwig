61247bd02111ecce94e2d5325577a3056083edc3
Andy Wang <andynmnwang@gmail.com>
2020-09-12 00:49:56 -0500

    use mongodb rather than mongo

--- server/app/index.js
+++ server/app/index.js
@@ -1,18 +1,22 @@
 const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const mongodb = require('mongodb')
 
 const apiRoutes = require('../src/controllers')
+
+const database;
 
 const run = (port, dburi) => {
   const app = express()
   const server = http.createServer(app)
+  const MongoClient = mongodb.MongoClient
 
   app.use(bodyParser.json())
   app.use('/api/v1', apiRoutes)
 
-  mongoose.connect(dburi, { useNewUrlParser: true, useUnifiedTopology: true }).then( () => {
+  MongoClient.connect(dburi).then( (db) => {
+    database = db
     server.listen(port, () => {
       console.log('Listening on http://localhost:' + port)
     })
@@ -23,5 +27,8 @@ const run = (port, dburi) => {
   })
 }
 
-module.exports = run
+module.exports = {
+  run: run,
+  database: database
+}
 
