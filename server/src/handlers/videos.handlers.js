f12622c1cffd01b5481378655737728aa51bb725
Andy Wang <andynmnwang@gmail.com>
2020-09-12 01:24:50 -0500

    video read/write works

--- server/src/handlers/videos.handlers.js
+++ server/src/handlers/videos.handlers.js
@@ -1,31 +1,62 @@
+const config = require('../../config')
 const mongodb = require('mongodb')
 const { Readable } = require('stream')
+
+const MongoClient = mongodb.MongoClient
 const ObjectId = mongodb.ObjectID
 
-const database = require('../../app').database
+var database;
+var bucket;
+MongoClient.connect(config.db.uri).then( (client) => {
+  database = client.db('musync')
+  bucket = new mongodb.GridFSBucket(database, {
+    bucketName: 'videos'
+  })
+}).catch( err => {
+  console.log('Error loading DB')
+  console.log(err)
+  process.exit(1)
+})
 
-const uploadMergedVideo = (name, file) => {
+const uploadMergedVideo = async (name, file) => {
   const readableTrackStream = new Readable()
   readableTrackStream.push(file.buffer)
   readableTrackStream.push(null)
-
-  let bucket = new mongodb.GridFSBucket(database, {
-    bucketName: 'videos'
-  })
 
   let uploadStream = bucket.openUploadStream(name)
   let id = uploadStream.id
   readableTrackStream.pipe(uploadStream)
 
-  uploadStream.on('error', () => {
-    throw new Error()
+  return new Promise( (resolve, reject) => {
+    uploadStream.on('error', error => {
+      reject(error)
+    })
+  
+    uploadStream.on('finish', () => {
+      resolve(id)
+    })
   })
+}
 
-  uploadStream.on('finish', () => {
-    return id
+const getVideoById = async (videoId) => {
+  let id = new ObjectId(videoId)
+  let downloadStream = bucket.openDownloadStream(id)
+
+  return new Promise( (resolve, reject) => {
+    let chunks = []
+    downloadStream.on('data', (chunk) => {
+      chunks.push(chunk)
+    })
+    downloadStream.on('error', error => {
+      reject(error)
+    })
+    downloadStream.on('end', () => {
+      resolve(chunks)
+    })
   })
 }
 
 module.exports = {
-  uploadMergedVideo: uploadMergedVideo
+  uploadMergedVideo: uploadMergedVideo,
+  getVideoById: getVideoById
 }
