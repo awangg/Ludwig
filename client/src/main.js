67118f7ad09312b87438596b6bf3e13276c9c6d3
clairexu5 <31214483+clairexu5@users.noreply.github.com>
2020-09-12 01:45:44 -0400

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 
-Vue.config.productionTip = false
+//Vue.config.productionTip = false
 
+/**
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+*/
+
+Vue.use(ElementUI);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
