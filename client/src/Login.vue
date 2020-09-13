270ddfb2e35dbcddaa7be14d2cd8407cafc9c205
megancx <megancxiao@gmail.com>
2020-09-12 22:48:06 -0400

    login

--- client/src/Login.vue
+++ client/src/Login.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="login">
-      <h1>Welcome Back</h1>
-      <el-row gutter="20">
-          <el-col :span="10">
-            <el-form ref="form" :model="loginList" label-width="120px">
-                <el-form-item label="Email">
-                    <el-input v-model="loginList.email"></el-input>
-                </el-form-item>
-                <el-form-item label="Password">
-                    <el-input v-model="loginList.password"></el-input>
-                </el-form-item>
-            </el-form>
-            <el-button>UNFINISHED (SUBMIT)</el-button>
-          </el-col>
-      </el-row>
+      <div class="login-page">
+  <div class="form">
+    <form class="login-form">
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <a href='/login'><el-button round type="circle" src="button" class="button">login</el-button></a>
+      <p class="message">Not registered? <a href="/signup">Create an account</a></p>
+    </form>
+  </div>
+</div>
   </div>
 </template>
 
@@ -40,32 +36,86 @@ export default {
 </script>
 
 <style>
-body {
-  direction: ltr;
-  margin: 0;
-  padding: 0;
-  background-color:#ffffff;
-  /* make it look decent enough #C8E7F5*/
-  color: #000000;
-  overflow-x: hidden;
+.login-page {
+  width: 360px;
+  padding: 8% 0 0;
+  margin: auto;
+}
+.form {
   position: relative;
-  height: 100%; 
-}
-#home {
-  font-family: "./assets/TeX-Gyre-Adventor/texgyreadventor-regular.otf";
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  z-index: 1;
+  background: #FFFFFF;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 45px;
   text-align: center;
-  color: #000000;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
 }
-@media screen and (max-width: 399px) {
-  body {
-    overflow: visible;
-    overflow-x: hidden;
-  }
+.form input {
+  font-family: Helvetica, sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
 }
-#item {
-  margin: 0;
+.form button {
+  font-family: Helvetica, sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #C998C3;
+  width: 100%;
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.form button:hover,.form button:active,.form button:focus {
+  background: #E4CEE0;
+}
+.form .message {
+  margin: 15px 0 0;
+  color: #b3b3b3;
+  font-size: 12px;
+}
+.form .message a {
+  color: #C998C3;
+  text-decoration: none;
+}
+
+.container {
+  position: relative;
+  z-index: 1;
+  max-width: 300px;
+  margin: 0 auto;
+}
+.container:before, .container:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+.container .info {
+  margin: 50px auto;
+  text-align: center;
+}
+.container .info h1 {
+  margin: 0 0 15px;
   padding: 0;
+  font-size: 36px;
+  font-weight: 300;
+  color: #1a1a1a;
+}
+
+body {
+  background: #E4CEE0; 
+  font-family: Helvetica, sans-serif, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;      
 }
 </style>
