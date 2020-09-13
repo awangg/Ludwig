3e31cf48110088e7f08eb72d7d86c5af2779311c
Andy Wang <andynmnwang@gmail.com>
2020-09-13 01:34:44 -0500

    add classCode to payload

--- server/src/handlers/auth.handler.js
+++ server/src/handlers/auth.handler.js
@@ -15,11 +15,11 @@ const login = async (email, password) => {
       exp: moment.unix(moment().add('30', 'm')),
       sub: currUser._id,
     }
-    console.log(payload)
     return {
       roles: currUser.roles,
       name: currUser.name,
       email: currUser.email,
+      classCode: currUser.classes[0],
       token: jwt.encode(payload, config.auth.secret)
     }
   } else {
