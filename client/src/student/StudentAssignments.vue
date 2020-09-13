6927cb9c11fb2d9ae8cef018e258433d49afd208
megancx <megancxiao@gmail.com>
2020-09-13 02:26:01 -0400

    update

--- client/src/student/StudentAssignments.vue
+++ client/src/student/StudentAssignments.vue
@@ -4,10 +4,11 @@
      <el-col :span="11"><div class="grid-content">
         <span>
         <div class="title">
-          Assignment
+          Assignments
         </div>
         </span>
       </div></el-col>
+      <!--need to get background color to not cover over navbar-->
       <div id="background"><div class = "grid-content2">
 
       <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans&display=swap" rel="stylesheet">
@@ -34,23 +35,32 @@ export default {
 .navbar {
   margin-top:0px;
 }
+
+.title{
+  font-family: 'Kumbh Sans', sans-serif;
+  font-size: 24px;
+}
 .logo {
   height: 80px;
   width: 130px;
   position: left;
   float:left;
 }
+.grid-content{
+  margin-top:6%;
+}
 
-.button {
-  background-color: #C998C3;
-  color: #ffffff;
-  font-family: Arial, Helvetica, sans-serif;
-  text-align: center;
-  font-size: 24px;
-  width: 200px;
-  height: 75px;
-  margin-top: 100px;
-  padding-bottom: 9px; 
+.grid-content2 {
+  margin-top:20%;
+}
+
+#background {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 50%;
+    height: 100%;
+    background-color: #E4CEE0; 
 }
 
 </style> 
